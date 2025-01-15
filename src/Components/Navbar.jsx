0069d875a1be3524dcb9e4675fb56bf70347c600
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-01-15 23:06:22 +0200

    adding hovering effects

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,11 +5,38 @@ const Navbar = () => {
         <div className='flex justify-between items-center h-24 w-100%'>
             <h1 className=' font-bold uppercase text-2xl'>plants for plans</h1>
             <ul className='flex items-center gap-x-14 h-16'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Content</li>
-                <button >Log In</button>
-                <button className='w-20 h-10 font-medium text-black ' style={{ background: '#f5f5dc' }}>Sign Up</button>
+                <li>
+                    <button class="relative group  py-1.5 px-2.5 ">
+                        <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-yellow-50 group-hover:w-full group-hover:transition-all">
+                        </span>
+                        Home
+                    </button>
+                </li>
+                <li>
+                    <button class="relative group  py-1.5 px-2.5 ">
+                        <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-yellow-50 group-hover:w-full group-hover:transition-all">
+                        </span>
+                        About
+                    </button>
+                </li>
+                <li>
+                    <button class="relative group  py-1.5 px-2.5 ">
+                        <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-yellow-50 group-hover:w-full group-hover:transition-all">
+                        </span>
+                        Contact
+                    </button>
+                </li>
+                <li>
+                    <button class="relative group  py-1.5 px-2.5 ">
+                        <span class="absolute bottom-0 left-0 w-0 h-0.5 bg-yellow-50 group-hover:w-full group-hover:transition-all">
+                        </span>
+                        Login
+                    </button>
+                </li>
+                <button type="button" class="w-20 h-10 text-nowrap inline-flex items-center justify-center border-2 bg-[#f5f5dc] border-transparent px-12 py-3 text-center text-base font-bold text-black transition-all duration-200 ease-in-out focus:shadow hover:border-[#f5f5dc] hover:bg-transparent hover:text-[#f5f5dc]">
+                    Sign Up
+                </button>
+
             </ul>
         </div>
 
