0b8d735da97296196a206af4694535be74df6b8c
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-01-16 17:33:31 +0200

    adding changing text part 2

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
 
 const Main = () => {
+    function heroTextChanger() {
+        let firstWord, secondWord, thirdWord, forthWord;
+        let toggle = false;
+        //put a time counter that count then change a boolean variable to change the words of the hero
+        setInterval(() => {
+            toggle = !toggle;
+
+            if (toggle == true) {
+                firstWord = "STOP"
+                secondWord = "WASTING"
+                thirdWord = "YOUR"
+                forthWord = "TIME"
+            }
+            else {
+                firstWord = "START"
+                secondWord = "GETTING"
+                thirdWord = "JOB"
+                forthWord = "DONE"
+            }
+            document.getElementById("hero-first-word").textContent = firstWord;
+            document.getElementById("hero-second-word").textContent = secondWord;
+            document.getElementById("hero-third-word").textContent = thirdWord;
+            document.getElementById("hero-forth-word").textContent = forthWord;
+        }, 7000)
+
+    }
+    heroTextChanger();
     return (
         <div className='flex  w-100% items-center h-[calc(100vh-8rem)]'>
             <section className='w-1/2 flex justify-center flex-col'>
                 <div style={{ fontSize: '150px' }} className=' p-0 m-0 flex flex-col leading-none font-bold'>
-                    <div>STOP</div>
-                    <div>WASTING</div>
-                    <div>YOUR</div>
-                    <div style={{ color: '#60e05c' }}>TIME</div>
+                    <div id='hero-first-word'>STOP</div>
+                    <div id='hero-second-word'>WASTING</div>
+                    <div id='hero-third-word'>YOUR</div>
+                    <div id='hero-forth-word' style={{ color: '#60e05c' }}>TIME</div>
                 </div>
                 <p>this is where the big header will be added</p>
             </section>
