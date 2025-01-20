0279c12c864d7aab8fcd2eaeac9bb845371e3509
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-01-20 02:56:34 +0200

    adding cards to the Plans.jsx

--- src/Components/Plans.jsx
+++ src/Components/Plans.jsx
@@ -31,7 +31,32 @@ const Plans = () => {
                 <div className='w-[40px] h-[40px] bg-[#f5f5dc]'></div>
                 <div className='w-[40px] h-[40px] bg-[#f5f5dc]'></div>
             </div>
-            <div className='the-content flex w-[100%] bg-[#f5f5dc] h-[500px]'></div>
+            {/* the content of the plans.jsx */}
+            <div className='the-content-container flex w-[100%] bg-[#f5f5dc] h-[700px]'>
+                <div className='the-content w-[100%] mx-[15%] my-[40px] flex justify-center flex-col ' >
+                    <h1 className='text-[#1f1f1f] text-4xl uppercase flex justify-center items-center mb-[60px] ' style={{ fontFamily: 'Poppins, sans-serif', fontWeight: '700' }}>Choose Your Working Time!</h1>
+                    <div className='flex justify-around items-center '>
+                        <div className='relative flex h-[450px] w-[400px] bg-[#1f1f1f]'>
+                            <div className='absolute top-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-left corner */}
+                            <div className='absolute top-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-right corner */}
+                            <div className='absolute bottom-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-left corner */}
+                            <div className='absolute bottom-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-right corner */}
+                        </div>
+                        <div className='relative flex h-[450px] w-[400px] bg-[#1f1f1f]'>
+                            <div className='absolute top-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-left corner */}
+                            <div className='absolute top-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-right corner */}
+                            <div className='absolute bottom-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-left corner */}
+                            <div className='absolute bottom-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-right corner */}
+                        </div>
+                        <div className='relative flex h-[450px] w-[400px] bg-[#1f1f1f]'>
+                            <div className='absolute top-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-left corner */}
+                            <div className='absolute top-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-right corner */}
+                            <div className='absolute bottom-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-left corner */}
+                            <div className='absolute bottom-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-right corner */}
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className='wrapper flex w-[100%] justify-between items-center'>
                 <div className='w-[40px] h-[40px] bg-[#f5f5dc]'></div>
                 <div className='w-[40px] h-[40px] bg-[#f5f5dc]'></div>
