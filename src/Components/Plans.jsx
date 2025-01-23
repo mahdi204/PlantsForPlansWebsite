11bc6062d4846ed3cc904498a04c65ae4f1c5953
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-01-23 14:10:28 +0200

    editing plans blocks explanation

--- src/Components/Plans.jsx
+++ src/Components/Plans.jsx
@@ -33,26 +33,41 @@ const Plans = () => {
             </div>
             {/* the content of the plans.jsx */}
             <div className='the-content-container flex w-[100%] bg-[#f5f5dc] h-[700px]'>
-                <div className='the-content w-[100%] mx-[15%] my-[40px] flex justify-center flex-col ' >
+                <div
+                    className='the-content w-[100%] mx-[15%] my-[40px] flex justify-center flex-col '
+                    style={{ fontFamily: "poppins", fontWeight: 600 }}
+                >
                     <h1 className='text-[#1f1f1f] text-4xl uppercase flex justify-center items-center mb-[60px] ' style={{ fontFamily: 'Poppins, sans-serif', fontWeight: '700' }}>Choose Your Working Time!</h1>
                     <div className='flex justify-around items-center '>
-                        <div className='relative flex h-[450px] w-[400px] bg-[#1f1f1f]'>
-                            <div className='absolute top-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-left corner */}
-                            <div className='absolute top-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-right corner */}
-                            <div className='absolute bottom-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-left corner */}
-                            <div className='absolute bottom-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-right corner */}
+                        <div className='flex flex-col items-center justify-center h-[500px] w-[400px]  '>
+                            <div className='flex flex-col items-center justify-centerw-[100%] h-[200px]'>
+                                <img
+                                    className='max-w-[100px] h-auto object-contain'
+                                    src="/images/iso-block.png"
+                                    alt="alt image"
+                                />
+                            </div>
+                            <p className='text-4xl text-[#1f1f1f]'>+5 Minutes</p>
                         </div>
-                        <div className='relative flex h-[450px] w-[400px] bg-[#1f1f1f]'>
-                            <div className='absolute top-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-left corner */}
-                            <div className='absolute top-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-right corner */}
-                            <div className='absolute bottom-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-left corner */}
-                            <div className='absolute bottom-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-right corner */}
+                        <div className='flex flex-col items-center justify-center h-[500px] w-[400px] '>
+                            <div className='flex flex-col items-center justify-centerw-[100%] h-[200px]'>
+                                <img
+                                    className='max-w-[150px] h-auto object-contain'
+                                    src="/images/iso-blocks-4.png"
+                                    alt="alt image"
+                                />
+                            </div>
+                            <p className='text-4xl text-[#1f1f1f]'>+1 Hour</p>
                         </div>
-                        <div className='relative flex h-[450px] w-[400px] bg-[#1f1f1f]'>
-                            <div className='absolute top-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-left corner */}
-                            <div className='absolute top-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Top-right corner */}
-                            <div className='absolute bottom-0 left-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-left corner */}
-                            <div className='absolute bottom-0 right-0 w-[10px] h-[10px] bg-[#f5f5dc]'></div> {/* Bottom-right corner */}
+                        <div className='flex flex-col items-center justify-center h-[500px] w-[400px] '>
+                            <div className='flex flex-col items-center justify-centerw-[100%] h-[200px]'>
+                                <img
+                                    className='max-w-[150px] h-auto object-contain'
+                                    src="/images/iso-blocks-9.png"
+                                    alt="alt image"
+                                />
+                            </div>
+                            <p className='text-4xl text-[#1f1f1f]'>+2 Hours</p>
                         </div>
                     </div>
                 </div>
