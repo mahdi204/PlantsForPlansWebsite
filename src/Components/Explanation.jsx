ef8b90783590d53e5a9b1903251f162db850ab6f
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-01-24 23:52:36 +0200

    explanation

--- src/Components/Explanation.jsx
+++ src/Components/Explanation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Explanation = () => {
     return (
-        <div className='flex items-center justify-center h-[100vh] bg-[#1f1f1f]]'>
+        <div className='flex ml-60 mr-60 items-center justify-center h-[100vh] bg-[#1f1f1f]]'>
             <p className='text-5xl'>Explanation</p>
         </div>
     )
