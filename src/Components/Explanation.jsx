893f1d01320d653a447b80e9a3a6d02ad579979c
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-01-25 21:43:29 +0200

    adding some useStates

--- src/Components/Explanation.jsx
+++ src/Components/Explanation.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Explanation = () => {
+    const [plantImage, setPlnatImage] = useState({
+        first: true,
+        second: false,
+        third: false,
+        forth: false
+    })
     return (
         <div className='flex ml-60 mr-60 items-center justify-between h-[100vh] bg-[#1f1f1f]]'>
             <div className=''>
-                <div>
+                <div className='relative'>
+                    <img
+                        className="absolute max-w-[200px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-auto object-contain"
+                        src="/images/mobile-plant-1.png"
+                        alt="block"
+                    />
                     <img
                         className="max-w-[400px] h-auto object-contain"
                         src="/images/pixel-mobile.png"
