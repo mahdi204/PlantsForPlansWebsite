4ab4d4a9844b8e82cfa255a94c03989093b5fa1f
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-01-25 19:13:11 +0200

    adding pixel phone image

--- src/Components/Explanation.jsx
+++ src/Components/Explanation.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 const Explanation = () => {
     return (
         <div className='flex ml-60 mr-60 items-center justify-between h-[100vh] bg-[#1f1f1f]]'>
-            <div><p>Right Div</p></div>
-            <div><p>Left Div</p></div>
+            <div className=''>
+                <div>
+                    <img
+                        className="max-w-[400px] h-auto object-contain"
+                        src="/images/pixel-mobile.png"
+                        alt="block"
+                    />
+                </div>
+            </div>
+            <div className='flex items-center justify-center w-[60%] '>
+                <h2 className='text-6xl'>Stop Scrolling Your Phone</h2>
+                <p>Set your plan time and stick to your work, while giving the plant time to grow.</p>
+            </div>
         </div>
     )
 }
