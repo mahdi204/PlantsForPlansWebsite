a5ef67b06d5a648fa2435eb4128b509ee59c8f9c
Mahdi <133682620+mahdi204@users.noreply.github.com>
2025-01-26 16:24:25 +0200

    editing Foot.js

--- src/Components/Foot.jsx
+++ src/Components/Foot.jsx
@@ -8,13 +8,16 @@ const Foot = () => {
             style={{ fontFamily: 'poppins', fontWeight: 200 }}
         >
             <div className='flex justify-end items-end w-[100%] bg-[#1f1f1f] h-[100px]  '>
-                <div className='flex justify-end items-end h-[100%] w-[300px]  text-[#ece7d5]'>
-                    <img
-                        className='max-w-[190px] h-auto object-contain'
-                        src="/images/Footer-Plants.png"
-                        alt="footer plants"
-                    />
+                <div className='mr-60 ml-60'>
+                    <div className='flex justify-end items-end h-[100%] w-[300px]  text-[#ece7d5]'>
+                        <img
+                            className='max-w-[190px] h-auto object-contain'
+                            src="/images/Footer-Plants-2.png"
+                            alt="footer plants"
+                        />
+                    </div>
                 </div>
+
             </div>
             <div className='flex ml-60 mr-60 h-[300px]' style={{ fontWeight: 600 }} >
                 <p className='text-4xl'>This is the Footer</p>
